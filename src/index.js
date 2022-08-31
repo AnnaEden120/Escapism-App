59075db1a977aeb62da44f666f72dedb77e09660
Anna Edenburg <107410076+AnnaEden120@users.noreply.github.com>
2022-08-31 11:25:02 +0300

    Trying to create routes

--- src/index.js
+++ src/index.js
@@ -2,24 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  BrowserRouter as Route,
-  Routes,
-  Router,
-  Switch
-} from "react-router-dom";
-import MyList from "./pages/MyList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Switch>
-          <Route path="/" component={App} />
-          <Route path="/pages/mylist" component={MyList} />
-        </Switch>
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <React.Fragment>
+    <App />
+  </React.Fragment>
 );
