d27f35a8167668c071f207dd14ff838bb56758bc
Anna Edenburg <107410076+AnnaEden120@users.noreply.github.com>
2022-09-03 10:36:28 +0300

    onclick form on home page

--- src/Home.js
+++ src/Home.js
@@ -3,26 +3,18 @@ import Form from "./components/Form";
 import { useState } from "react";
 
 function Home() {
-  const [link, updateLink] = useState(null);
-  const [copied, setCopied] = useState(false);
-  const generateLink = (data) => {
-    const newLink = `postgres://${data.username}:${data.password}@${
-      data.host
-    }:${data.port}${data.db ? "/final-app" + data.db : "user_sign"}`;
-    return updateLink(newLink);
-  };
+  const [isShown, setIsShown] = useState(false);
 
-  const handleClick=()=>{
-    if (onClick=1){
-      getFunc
-    }
-    )
-  }
+  const handleClick = (event) => {
+    // 👇️ toggle shown state
+    setIsShown((current) => true);
+  };
 
   return (
     <div>
       <h1>Home</h1>
-  <button onClick={handleClick(<Form/>)}>Sign up</button>
+      <button onClick={handleClick}>Sign up</button>
+      {isShown && <Form />}
     </div>
   );
 }
